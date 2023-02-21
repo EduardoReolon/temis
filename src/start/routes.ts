635685d248b7372c829430ad3fb3fc13c66cd8b1
EduardoReolon <eduardo_reolon@hotmail.com>
2023-02-21 12:36:00 -0300

    ...

--- src/start/routes.ts
+++ src/start/routes.ts
@@ -4,10 +4,10 @@ import Route from "../kernel/routehandler";
 Route.group([
   Route.post('update/:id', 'AuthController.update'),
   Route.group([
-    Route.get('me/', 'AuthController.me'),
+    Route.get('me', 'AuthController.me'),
   ]),
 ])
-  .prefix('/api/v1/auth/')
+  .prefix('api/v1/auth')
   .middleware(['Auth']);
 
 Route.get('*', async ({params, response}: HttpContextContract) => {response.status(201).send(params)})
